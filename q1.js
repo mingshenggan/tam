2155358916384a311a4d96b99432eab05ad0cf06
Mingsheng Gan <mingshengg@outlook.com>
2025-04-14 14:54:35 +0800

    Adapt original to use stripe node

--- q1.js
+++ q1.js
@@ -1,6 +1,8 @@
+import Stripe from 'stripe';
+
 // Set your secret key. Remember to switch to your live secret key in production.
 // See your keys here: https://dashboard.stripe.com/apikeys
-const stripe = require('stripe')('sk_test_51RDgKlPoK1psmfNnLDTqbFDCCTnddqdvQxi8M0rbAeL79uSirxQQe1xZhFvShmrvf7vFeyvP9fEY9xQMY3bqElla002Slyyl7G');
+const stripe = new Stripe('sk_test_51RDgKlPoK1psmfNnLDTqbFDCCTnddqdvQxi8M0rbAeL79uSirxQQe1xZhFvShmrvf7vFeyvP9fEY9xQMY3bqElla002Slyyl7G');
 
 const customer = await stripe.customers.create({
   payment_method: 'pm_card_visa',
@@ -8,5 +10,3 @@ const customer = await stripe.customers.create({
   name: 'TAM Candidate Mingsheng',
 });
 console.log('Customer created. Cust ID: ', customer.id)
-
-
